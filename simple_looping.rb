ee76ce86d8a949da937ce21500ac082d304c5bb6
Lindsey Wells <lindsey.wells84@gmail.com>
2019-04-19 20:07:04 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -1,5 +1,13 @@
 def loop_iterator(number_of_times)
-
+counter = 0
+  	  loop do
+  counter <= number_of_times
+puts "Welcome to Flatiron School's Web Development Course!"
+ counter += 1
+ if counter >= number_of_times
+   break
+   end
+   end
   
   
 def times_iterator(number_of_times)
