a86e5df1729ee5af32b29ab5e400b0b871b0a8c4
Lindsey Wells <lindsey.wells84@gmail.com>
2019-04-19 20:11:31 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -9,6 +9,13 @@ puts "Welcome to Flatiron School's Web Development Course!"
    end
  end
  
- 
+ def times_iterator(number_of_times)
+  # code your solution here using the "times" keyword
+  phrase = "Welcome to Flatiron School's Web Development Course!"
+ number_of_times.times do
+   puts phrase
+   
+end
+end
   
   
